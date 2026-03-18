38ceba5951bf28a70011b6a110610052e5de57c0
Iosif <iosifsotiriadis@gmail.com>
2026-03-19 01:13:51 +0200

    add CMakeLists.txt for cross-platform build

---
+++ build/CMakeFiles 2/ASBCodeV2.elf.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Startup/startup_stm32f446retx.s" "/Users/iosifsot/Documents/ASB-Supervisor/build/CMakeFiles/ASBCodeV2.elf.dir/Core/Startup/startup_stm32f446retx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Inc"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/can_mcu.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/can_mcu.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/can_mcu.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/main.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/stm32f4xx_it.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/syscalls.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/sysmem.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Core/Src/system_stm32f4xx.c" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "/Users/iosifsot/Documents/ASB-Supervisor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj" "gcc" "CMakeFiles/ASBCodeV2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
